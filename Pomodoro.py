dd79bdd0739aef23ede72cff9f21486442368d68
fr4ed <fr4ed@rustadholmboe.com>
2014-11-28 22:35:38 +0100

    Added simple sound

--- Pomodoro.py
+++ Pomodoro.py
@@ -1,25 +1,54 @@
 import time
+import pygame.mixer
+
 
 # To Do
 #
 # Skikkelige Ascii-tall
-# Mulighet for lyd ved stopp
 # Overkjøre/flash over screensaver
 
+
+def spillLydStart():
+    '''Spill en lyd'''
+    #
+    pygame.mixer.init()
+    ss=pygame.mixer.Sound('desktop-logout.ogg')
+    ss.play()
+
+
+def spillLydStopp():
+    '''Spill en lyd'''
+    #
+    pygame.mixer.init()
+    ss=pygame.mixer.Sound('phone-incoming-call.ogg')
+    ss.play()
+
+
 def pomodoro():
+    '''En Pomodoro på 25 minutter'''
+    #
     boom=60*25
-    print('Pomodoro starter')    
+    spillLydStart()
+    print('\n')*3
+    print('Pomodoro starter')
+    time.sleep(1)
     while boom > 0:
         time.sleep(1)
         if boom%60==0:
-            print('\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n     ' + str(int(boom/60)) + '\n \n \n ')
+            print('\n')*30
+            print(int(boom/60))
+            print('\n')*3
             #print(boom/60)
         boom -=1
     print('Pomodoro er over')
+    spillLydStopp()
 
 
 def pomPause():
+    '''5 minutter pause før neste Pomodoro'''
+    #
     boom=60*5
+    spillLydStart()
     print('Pausen starter')
     while boom > 0:
         time.sleep(1)
@@ -27,10 +56,14 @@ def pomPause():
             print(boom/60)
         boom -=1
     print('Pausen er over')
+    spillLydStopp()
+
 
 
 def startPomodoro():
-    starte = input('Starte Pomodoro?: ')
+    '''Start Pomodoro-rekke'''
+    #
+    starte = raw_input('Starte Pomodoro?: ')
     if starte in ['j','J','y','Y']:
         pomodoro()
         pomPause()
@@ -42,13 +75,19 @@ def startPomodoro():
         startPomodoro()
 
 
+# Kjør Pomodoro
+startPomodoro()
 
-print("""\n
 
- ____  ____  
-|___  \| ___| 
-  __)   |___  \ 
- /   __/ ___)  |
-|_____|____/ 
 
-  \n""")
+
+##
+##print("""\n
+##
+## ____  ____  
+##|___  \| ___| 
+##  __)   |___  \ 
+## /   __/ ___)  |
+##|_____|____/ 
+##
+##  \n""")
